b4bc569c1659a5b624b95e89f3dc2fe7bd7fd24c
Korthux <loury@nodalview.com>
2023-01-25 16:49:27 +0100

    fix mailing

--- app/mailers/simulation_mailer.rb
+++ app/mailers/simulation_mailer.rb
@@ -4,7 +4,7 @@ class SimulationMailer < ApplicationMailer
   def send_simulation_email
     @simulation = params[:simulation]
     puts(@simulation.first_name)
-    @simulation_url = params[:domain_name]+"/simulation?id="+@simulation.id
+    @simulation_url = params[:domain_name]+"/simulation?id="+@simulation.id.to_s
     mail(to: @simulation.email, subject: "Votre simulation")
   end
 end
